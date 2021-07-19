3e8c59a80ab0284a2d64c75254cf26eccf5305e3
Lenni <lennivh24@gmail.com>
2021-07-19 13:35:45 +0000

    blind test, works well

--- pkg/number/complex/cross_test.go
+++ pkg/number/complex/cross_test.go
@@ -2,16 +2,70 @@ package complex
 
 import (
 	"testing"
+	"testing/quick"
 
 	"github.com/scrouthtv/algobra/pkg/number/def"
 )
 
 func TestBasicCross(t *testing.T) {
-	a := New(def.NewReal(8), def.NewReal(12))
+	a := New(def.NewReal(9), def.NewReal(12))
 	n := def.NewReal(3)
 
-	x := New(def.NewReal(24), def.NewReal(36))
+	x := New(def.NewReal(12), def.NewReal(12))
+	if !n.Add(a).Equal(x) {
+		t.Errorf("Add failed %s + %s = %s, should be %s", n, a, n.Add(a), x)
+	}
+
+	x = New(def.NewReal(6), def.NewReal(12))
+	if !n.Subtract(a).Equal(x) {
+		t.Errorf("Subtract failed %s - %s = %s, should be %s", n, a, n.Subtract(a), x)
+	}
+
+	x = New(def.NewReal(27), def.NewReal(36))
 	if !n.Multiply(a).Equal(x) {
 		t.Errorf("Multiply failed: %s * %s = %s, should be %s", n, a, n.Multiply(a), x)
 	}
+
+	x = New(def.NewReal(3), def.NewReal(4))
+	ans, err := a.Divide(n)
+	if err != nil {
+		t.Errorf("Divide failed: %s / %s gave error %s, should be %s", a, n, err.Error(), x)
+	}
+	if !ans.Equal(x) {
+		t.Errorf("Divide failed: %s / %s = %s, should be %s", a, n, ans, x)
+	}
+}
+
+func TestBlindCross(t *testing.T) {
+	cfg := &quick.Config{}
+	quick.Check(testAllOps, cfg)
+}
+
+// testAllOps tests whether
+//  - (a+bi) + c == c + (a+bi)
+//  - (a+bi) - c == -(c + (a+bi))
+//  - (a+bi) * c == c * (a+bi)
+func testAllOps(a, b, c float64) bool {
+	z := New(def.NewReal(a), def.NewReal(b))
+	n := def.NewReal(c)
+
+	ansa := z.Add(n)
+	ansb := n.Add(z)
+	if !ansa.Equal(ansb) {
+		return false
+	}
+
+	ansa = z.Subtract(n)
+	ansb = n.Subtract(z)
+	if !ansa.Equal(ansb) {
+		return false
+	}
+
+	ansa = z.Multiply(n)
+	ansb = n.Multiply(z)
+	if !ansa.Equal(ansb) {
+		return false
+	}
+
+	return true
 }
--- pkg/number/complex/go.mod
+++ pkg/number/complex/go.mod
@@ -5,5 +5,5 @@ go 1.16
 require (
 	github.com/scrouthtv/algobra/pkg/number v0.0.0-20210719094258-0d702353e86c
 	github.com/scrouthtv/algobra/pkg/number/def v0.0.0-20210719092745-dacd8901e4c9
-	github.com/scrouthtv/algobra/pkg/number/fastreal v0.0.0-20210719090809-99d5d71785bf // indirect
+	github.com/scrouthtv/algobra/pkg/number/fastreal v0.0.0-20210719132347-62838339f4be // indirect
 )
