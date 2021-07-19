dacd8901e4c9361f87bc7e6401c3c224eb3f2851
Lenni <lennivh24@gmail.com>
2021-07-19 09:27:45 +0000

    zero & one constants

--- pkg/number/def/impl.go
+++ pkg/number/def/impl.go
@@ -1,7 +1,14 @@
 package def
 
-import "github.com/scrouthtv/algobra/pkg/number"
-import "github.com/scrouthtv/algobra/pkg/number/fastreal"
+import (
+	"github.com/scrouthtv/algobra/pkg/number"
+	"github.com/scrouthtv/algobra/pkg/number/fastreal"
+)
+
+var (
+	Zero = NewReal(0)
+	One  = NewReal(1)
+)
 
 // NewReal creates a new real number that uses the current default
 // implementation for real numbers.
